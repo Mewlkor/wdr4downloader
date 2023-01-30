e0a38c7216bb8effbf6083c1246074410c32a29c
Florian Gruß <gruss221@googlemail.com>
2023-01-30 19:57:52 +0100

    v1.1
    more chanels 
    variable timeframe

--- README.md
+++ README.md
@@ -1,7 +1,18 @@
 # wdr4downloader
-use install.sh to get dependencies
+Use install.sh to get dependencies
 
-script downloads 3:01h of http://wdr-wdr4-live.icecast.wdr.de/wdr/wdr4/live/mp3/128/stream.mp3
+Downloads these WDR streams:
+
+- 1Live: http://wdr-1live-live.icecast.wdr.de/wdr/1live/live/mp3/128/stream.mp3
+- WDR2: http://wdr-wdr2-ostwestfalenlippe.icecast.wdr.de/wdr/wdr2/ostwestfalenlippe/mp3/128/stream.mp3
+- WDR3: http://wdr-wdr3-live.icecast.wdr.de/wdr/wdr3/live/mp3/128/stream.mp3
+- WDR4: http://wdr-wdr4-live.icecast.wdr.de/wdr/wdr4/live/mp3/128/stream.mp3
+- WDR5: http://wdr-wdr5-live.icecast.wdr.de/wdr/wdr5/live/mp3/128/stream.mp3
+
+Usage:
+- First argument: Name of Radiochanel form list above
+- Second argument: Duration of recording in minutes
+- Third argument: Desired filename. The script will add the current date
 
 made for my father
 
--- main.py
+++ main.py
@@ -2,23 +2,31 @@ import requests
 import datetime
 import multiprocessing
 import time
+import sys
 
+wdr_streams = {'1Live': 'http://wdr-1live-live.icecast.wdr.de/wdr/1live/live/mp3/128/stream.mp3',
+                'WDR2': 'http://wdr-wdr2-ostwestfalenlippe.icecast.wdr.de/wdr/wdr2/ostwestfalenlippe/mp3/128/stream.mp3',
+                'WDR3': 'http://wdr-wdr3-live.icecast.wdr.de/wdr/wdr3/live/mp3/128/stream.mp3',
+                'WDR4': 'http://wdr-wdr4-live.icecast.wdr.de/wdr/wdr4/live/mp3/128/stream.mp3',
+                'WDR5': 'http://wdr-wdr5-live.icecast.wdr.de/wdr/wdr5/live/mp3/128/stream.mp3'}
 
-def download():
-    stream_url = 'http://wdr-wdr4-live.icecast.wdr.de/wdr/wdr4/live/mp3/128/stream.mp3'
+def download(stream_url=wdr_streams['1Live'] , filename='download'):
     stream_request = requests.get(stream_url, stream=True)
-    with open(f'WDR4_Klassik_populaer_{datetime.date.today()}.mp3', 'wb') as f:
+    with open(f'{filename}_{datetime.date.today()}.mp3', 'wb') as f:
         try:
             for block in stream_request.iter_content(1024):
                 f.write(block)
         except KeyboardInterrupt:
             pass
 
+
+
 if __name__ == '__main__':
-    t1 = multiprocessing.Process(target=download)
+    t1 = multiprocessing.Process(target=download, args=(wdr_streams[sys.argv[1]], sys.argv[3]))
     print('Starting download')
     t1.start()
-    time.sleep(10860)
+    wait_time = int(sys.argv[2])*60
+    time.sleep(wait_time)
     print('Stopping download')
     t1.terminate()
     t1.join()
