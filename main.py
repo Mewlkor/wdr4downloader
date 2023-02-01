b54df8d9e69f212a32ceeae5826e441a699abbf0
Florian Gruß <gruss221@googlemail.com>
2023-02-01 22:27:27 +0100

    v 1.2
    - switched links to https
    - download writes to disk instantly
    - implementet safer way to stop download thread
    - download switched from process to thread

--- main.py
+++ main.py
@@ -1,32 +1,37 @@
-import requests
-import datetime
+import requests, datetime, time, os, sys, logging
 import multiprocessing
-import time
-import sys
+import threading
 
-wdr_streams = {'1Live': 'http://wdr-1live-live.icecast.wdr.de/wdr/1live/live/mp3/128/stream.mp3',
-                'WDR2': 'http://wdr-wdr2-ostwestfalenlippe.icecast.wdr.de/wdr/wdr2/ostwestfalenlippe/mp3/128/stream.mp3',
-                'WDR3': 'http://wdr-wdr3-live.icecast.wdr.de/wdr/wdr3/live/mp3/128/stream.mp3',
-                'WDR4': 'http://wdr-wdr4-live.icecast.wdr.de/wdr/wdr4/live/mp3/128/stream.mp3',
-                'WDR5': 'http://wdr-wdr5-live.icecast.wdr.de/wdr/wdr5/live/mp3/128/stream.mp3'}
+stop_queue = multiprocessing.Queue()
 
-def download(stream_url=wdr_streams['1Live'] , filename='download'):
+wdr_streams = {'1Live': "https://wdr-1live-live.icecastssl.wdr.de/wdr/1live/live/mp3/128/stream.mp3",
+                'WDR2': "https://wdr-wdr2-ostwestfalenlippe.icecastssl.wdr.de/wdr/wdr2/ostwestfalenlippe/mp3/128/stream.mp3",
+                'WDR3': "https://wdr-wdr3-live.icecastssl.wdr.de/wdr/wdr3/live/mp3/128/stream.mp3",
+                'WDR4': "https://wdr-wdr4-live.icecastssl.wdr.de/wdr/wdr4/live/mp3/128/stream.mp3",
+                'WDR5': "https://wdr-wdr5-live.icecastssl.wdr.de/wdr/wdr5/live/mp3/128/stream.mp3"}
+
+
+def download(stream_url , filename='download'):
     stream_request = requests.get(stream_url, stream=True)
-    with open(f'{filename}_{datetime.date.today()}.mp3', 'wb') as f:
+    with open(f'{filename}_{datetime.date.today()}.mp3', 'wb', 0) as f:
         try:
-            for block in stream_request.iter_content(1024):
-                f.write(block)
+            for block in stream_request.iter_content(1024): # Write 1024 bytes at a time
+                f.write(block)                              # Write to file
+                f.flush()                                   # Flush to disk
+                os.fsync(f)                                 # Force os to write to disk
+                if(stop_queue.qsize() == 1):                # Check if stop_queue is not empty, if so, raise KeyboardInterrupt to stop download. Check after every block is written to file
+                    raise KeyboardInterrupt
+               
+                
         except KeyboardInterrupt:
             pass
 
-
-
 if __name__ == '__main__':
-    t1 = multiprocessing.Process(target=download, args=(wdr_streams[sys.argv[1]], sys.argv[3]))
-    print('Starting download')
+    t1 = threading.Thread(target=download, args=(wdr_streams[sys.argv[1]], sys.argv[3]))
+    logging.info('Starting download')
     t1.start()
-    wait_time = int(sys.argv[2])*60
-    time.sleep(wait_time)
-    print('Stopping download')
-    t1.terminate()
+    wait_time = int(sys.argv[2])*60 # Convert minutes to seconds
+    time.sleep(wait_time) # Wait for the specified amount of time
+    logging.info('Stopping download')
+    stop_queue.put(1) # Put something in the queue to stop the download
     t1.join()
